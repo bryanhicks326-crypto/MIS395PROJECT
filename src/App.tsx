ade26f9d33f3dc824fe451aab161b219b65f4533
bryanhicks326-crypto <bryanhicks326@gmail.com>
2026-09-19 13:07:53 -0500

    Import React in App component
    Added missing import for React in App component.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,19 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import Gallery from './components/Gallery';
+1  import React from 'react';
+2  
+3  function App() {
+4    return (
+5      <div>
+6        <h1>Maddow Modular</h1>
+7        <p>Welcome to the MIS395 Project!</p>
+8      </div>
+9    );
+10 }
+11 
+12 export default App;
+
 import Locations from './components/Locations';
 import CTA from './components/CTA';
 import Footer from './components/Footer';
